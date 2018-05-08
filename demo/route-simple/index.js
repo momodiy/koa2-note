a5077f6b60b8c34ffdf2d03c63813bc0a4aaf25a
steven <qcjy16@126.com>
2018-05-08 21:24:21 +0800

    :pencil: make reconstruction with es6

--- demo/route-simple/index.js
+++ demo/route-simple/index.js
@@ -5,20 +5,15 @@ const app = new Koa()
 /**
  * 用Promise封装异步读取文件方法
  * @param  {string} page html文件名称
- * @return {promise}      
+ * @return {promise}
  */
-function render( page ) {
-  return new Promise(( resolve, reject ) => {
-    let viewUrl = `./view/${page}`
-    fs.readFile(viewUrl, "binary", ( err, data ) => {
-      if ( err ) {
-        reject( err )
-      } else {
-        resolve( data )
-      }
-    })
-  })
-}
+const render = page =>
+    new Promise((resolve, reject) => {
+        let viewUrl = `./view/${page}`
+        fs.readFile(viewUrl, "binary", (err, data) => {
+            err ? reject(err) : resolve(data)
+        })
+    });
 
 
 /**
@@ -26,35 +21,34 @@ function render( page ) {
  * @param  {string} url koa2上下文的url，ctx.url
  * @return {string}     获取HTML文件内容
  */
-async function route( url ) {
-  let view = '404.html'
-  switch ( url ) {
-    case '/':
-      view = 'index.html'
-      break
-    case '/index':
-      view = 'index.html'
-      break
-    case '/todo':
-      view = 'todo.html'
-      break
-    case '/404':
-      view = '404.html'
-      break
-    default:
-      break
-  }
-  let html = await render( view )
-  return html
+const route = url => {
+    let view = '';
+    switch (url) {
+        case '/':
+            view = 'index.html'
+            break
+
+        case '/index':
+            view = 'index.html'
+            break
+        case 'todo':
+            view = 'todo.html'
+            break
+        default:
+            view = '404.html'
+            break
+    }
+
 }
 
-app.use(async ctx=> {
-  let url = ctx.request.url
-  let html = await route( url )
-  ctx.body = html
+// app.use(fn)  相当于加载时执行的函数，执行结束后才继续向下执行
+app.use(async ctx => {
+    let url = ctx.request.url
+    let html = await route(url)
+    ctx.body = html
 })
 
 app.listen(3000, () => {
-  console.log('[demo] route-simple is starting at port 3000')
+    console.log('[demo] route-simple is starting at port 3000')
 })
 
