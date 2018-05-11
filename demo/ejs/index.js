08f6e1efd47f75a38218fb407a2591de48097ba8
steven <qcjy16@126.com>
2018-05-12 00:00:24 +0800

    :pencil: add some description （koa-view）

--- demo/ejs/index.js
+++ demo/ejs/index.js
@@ -3,18 +3,19 @@ const views = require('koa-views')
 const path = require('path')
 const app = new Koa()
 
+//koa-views 不支持自定义扩展名，这里必须为 ejs
 app.use(views(path.join(__dirname, './view'), {
-  extension: 'ejs'
+    extension: 'ejs' //
 }))
 
-app.use(async ctx=> {
-  let title = 'hello koa2'
-  await ctx.render('index', {
-    title,
-  })
+app.use(async ctx => {
+    let title = 'hello koa2'
+    await ctx.render('index', {
+        title,
+    })
 })
 
-app.listen(3000, ()=>{
-  console.log('[demo] ejs is starting at port 3000')
+app.listen(3000, () => {
+    console.log('[demo] ejs is starting at port 3000')
 })
 
