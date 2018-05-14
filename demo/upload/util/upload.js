40c6b561f6f6393d9cbd212f0910a625b14eeef0
steven <qcjy16@126.com>
2018-05-14 15:35:35 +0800

    :heavy_plus_sign: add some useful image（used in markdown files）

--- demo/upload/util/upload.js
+++ demo/upload/util/upload.js
@@ -8,15 +8,15 @@ const Busboy = require('busboy')
  * @param  {string} dirname 目录绝对地址
  * @return {boolean}        创建目录结果
  */
-function mkdirsSync( dirname ) {
-  if (fs.existsSync( dirname )) {
-    return true
-  } else {
-    if (mkdirsSync( path.dirname(dirname)) ) {
-      fs.mkdirSync( dirname )
-      return true
+const mkdirsSync = dirname => {
+    if (fs.existsSync(dirname)) {
+        return true
+    } else {
+        if (mkdirsSync(path.dirname(dirname))) {
+            fs.mkdirSync(dirname)
+            return true
+        }
     }
-  }
 }
 
 /**
@@ -24,75 +24,74 @@ function mkdirsSync( dirname ) {
  * @param  {string} fileName 获取上传文件的后缀名
  * @return {string}          文件后缀名
  */
-function getSuffixName( fileName ) {
-  let nameList = fileName.split('.')
-  return nameList[nameList.length - 1]
+const getSuffixName = fileName => {
+    let nameList = fileName.split('.')
+    return nameList[nameList.length - 1]
 }
 
 /**
  * 上传文件
  * @param  {object} ctx     koa上下文
  * @param  {object} options 文件上传参数 fileType文件类型， path文件存放路径
- * @return {promise}         
+ * @return {promise}
  */
-function uploadFile( ctx, options) {
-  let req = ctx.req
-  let res = ctx.res
-  let busboy = new Busboy({headers: req.headers})
+const uploadFile = (ctx, options) => {
+    let {req, res} = ctx;
+    let busboy = new Busboy({headers: req.headers})
 
-  // 获取类型
-  let fileType = options.fileType || 'common'
-  let filePath = path.join( options.path,  fileType)
-  let mkdirResult = mkdirsSync( filePath )
-  
-  return new Promise((resolve, reject) => {
-    console.log('文件上传中...')
-    let result = { 
-      success: false,
-      formData: {},
-    }
+    // 获取类型
+    let fileType = options.fileType || 'common'
+    let filePath = path.join(options.path, fileType)
+    let mkdirResult = mkdirsSync(filePath)
 
-    // 解析请求文件事件
-    busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
-      let fileName = Math.random().toString(16).substr(2) + '.' + getSuffixName(filename)
-      let _uploadFilePath = path.join( filePath, fileName )
-      let saveTo = path.join(_uploadFilePath)
+    return new Promise((resolve, reject) => {
+        console.log('文件上传中...')
+        let result = {
+            success: false,
+            formData: {},
+        }
 
-      // 文件保存到制定路径
-      file.pipe(fs.createWriteStream(saveTo))
+        // 解析请求文件事件
+        busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
+            let fileName = Math.random().toString(16).substr(2) + '.' + getSuffixName(filename)
+            let _uploadFilePath = path.join(filePath, fileName)
+            let saveTo = path.join(_uploadFilePath)
 
-      // 文件写入事件结束
-      file.on('end', function() {
-        result.success = true
-        result.message = '文件上传成功'
-        console.log('文件上传成功！')
-      })
+            // 文件保存到制定路径
+            file.pipe(fs.createWriteStream(saveTo))
+
+            // 文件写入事件结束
+            file.on('end', () => {
+                result.success = true
+                result.message = '文件上传成功'
+                console.log('文件上传成功！')
+            })
+        })
+
+        // 解析表单中其他字段信息
+        busboy.on('field', (fieldname, val, fieldnameTruncated, valTruncated, encoding, mimetype) => {
+            console.log('表单字段数据 [' + fieldname + ']: value: ' + inspect(val));
+            result.formData[fieldname] = inspect(val);
+        });
+
+        // 解析结束事件
+        busboy.on('finish', () => {
+            console.log('文件上传结束')
+            resolve(result)
+        })
+
+        // 解析错误事件
+        busboy.on('error', err => {
+            console.log('文件上出错 ' + err)
+            reject(result)
+        })
+
+        req.pipe(busboy)
     })
 
-    // 解析表单中其他字段信息
-    busboy.on('field', function(fieldname, val, fieldnameTruncated, valTruncated, encoding, mimetype) {
-      console.log('表单字段数据 [' + fieldname + ']: value: ' + inspect(val));
-      result.formData[fieldname] = inspect(val);
-    });
-
-    // 解析结束事件
-    busboy.on('finish', function( ) {
-      console.log('文件上结束')
-      resolve(result)
-    })
-
-    // 解析错误事件
-    busboy.on('error', function(err) {
-      console.log('文件上出错')
-      reject(result)
-    })
-
-    req.pipe(busboy)
-  })
-    
-} 
+}
 
 
-module.exports =  {
-  uploadFile
+module.exports = {
+    uploadFile
 }
